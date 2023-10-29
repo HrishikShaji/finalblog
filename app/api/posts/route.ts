b1d14213ed4fa433de3fdbaea5d26ab49592bbda
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-29 18:16:12 +0530

    minimal form

---
+++ app/api/posts/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/connect";
+
+export const GET = async (req: Request) => {
+  const { searchParams } = new URL(req.url);
+
+  const page = Number(searchParams.get("page"));
+  const cat = searchParams.get("cat") || null;
+  const featured = searchParams.get("editor") || null;
+  const popular = searchParams.get("popular") || null;
+  const POST_PER_PAGE = 3;
+
+  try {
+    const query = {
+      take: POST_PER_PAGE,
+      skip: POST_PER_PAGE * (page - 1),
+      where: {
+        ...(cat && { catSlug: cat }),
+        ...(featured && { featured: true }),
+      },
+      orderBy: {
+        ...(popular ? { views: "desc" } : { createdAt: "desc" }),
+      },
+      include: {
+        user: true,
+        votes: true,
+      },
+    };
+    const [posts, count] = await prisma.$transaction([
+      prisma.post.findMany(query as any),
+      prisma.post.count({ where: query.where }),
+    ]);
+    return new NextResponse(JSON.stringify({ posts, count }));
+  } catch (err) {
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong" }),
+    );
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
+import { baseUrl } from "@/lib/connect";
 
-export default function Home() {
+const fetchPosts = async (url: string) => {
+  const res = await fetch(url, {
+    cache: "no-store",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`Failed `);
+  }
+
+  return res.json();
+};
+
+export default async function Home() {
+  const data = await fetchPosts(`${baseUrl}/api/posts?page=1`);
+  console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       sample
+      <h1>{data.posts[0].title}</h1>
     </main>
   );
 }
