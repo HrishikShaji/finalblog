6274ba5045e543114e8431019dd7d856c3ddfb34
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-29 18:32:02 +0530

    featured added

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,9 @@
-import { baseUrl } from "@/lib/connect";
+import { Featured } from "@/components/Featured";
 
-const fetchPosts = async (url: string) => {
-  const res = await fetch(url, {
-    cache: "no-store",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (!res.ok) {
-    throw new Error(`Failed `);
-  }
-
-  return res.json();
-};
-
-export default async function Home() {
-  const data = await fetchPosts(`${baseUrl}api/posts?page=1`);
-
-  if (!data.posts) {
-    return <div>nothing</div>;
-  }
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      sample
-      <h1>{data.posts[0].title}</h1>
+      <Featured />
     </main>
   );
 }
---
+++ components/Featured.tsx
@@ -0,0 +1,30 @@
+import { baseUrl } from "@/lib/connect";
+
+const fetchPosts = async (url: string) => {
+  const res = await fetch(url, {
+    cache: "no-store",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`Failed `);
+  }
+
+  return res.json();
+};
+
+export const Featured = async () => {
+  const data = await fetchPosts(`${baseUrl}api/posts?page=1`);
+
+  if (!data.posts) {
+    return <div>nothing</div>;
+  }
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      sample
+      <h1>{data.posts[0].title}</h1>
+    </div>
+  );
+};
