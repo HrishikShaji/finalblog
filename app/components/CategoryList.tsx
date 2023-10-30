ffbe97519a56123ac7ccb834e4b5cd3ad888bb9e
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-30 22:20:41 +0530

    fetch error done

--- app/components/CategoryList.tsx
+++ app/components/CategoryList.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 
 export const CategoryList = async () => {
   const data = await fetchPosts(`${baseUrl}api/categories`);
+	if(!data){
+		return <div>Loading...</div>
+	}
   return (
     <div className="flex flex-col gap-5 py-10">
       <h1 className="text-3xl font-semibold">Popular categories</h1>
