3b623758773ae6bc567213989fa0b405f3635491
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-30 22:19:02 +0530

    nextResponse removed

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,13 +1,10 @@
-import { NextResponse } from "next/server";
 import prisma from "@/app/lib/connect";
 
 export const GET = async () => {
   try {
     const categories = await prisma.category.findMany();
-    return new NextResponse(JSON.stringify(categories));
+    return new Response(JSON.stringify(categories));
   } catch (err) {
-    return new NextResponse(
-      JSON.stringify({ message: "Something went wrong" }),
-    );
+    return new Response(JSON.stringify({ message: "Something went wrong" }));
   }
 };
