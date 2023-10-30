bac0e97c41a7618f26f04a3c5d55cf04253133e5
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-30 19:47:50 +0530

    featured api call added

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { baseUrl } from "../lib/connect";
 
 const fetchPosts = async (url: string) => {
@@ -17,12 +18,17 @@ const fetchPosts = async (url: string) => {
 
 export const Featured = async () => {
   const data = await fetchPosts(`${baseUrl}api/posts`);
-  console.log(data, baseUrl);
+  console.log("in the frontend", data);
+  if (!data.posts[0]) return <div>NO POSTS YET...</div>;
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      sample
-      {baseUrl}
-      {data.posts[0].title}
+    <div className="flex flex-col gap-10">
+      <h1 className="text-4xl">Discover my Stories and creative ideas</h1>
+      <div className="flex gap-4 relative h-[50vh] bg-neutral-600 justify-center items-center">
+        <h1 className="absolute z-10 text-4xl font-bold ">
+          {data.posts[0]?.title}
+        </h1>
+        <Link href={`posts/${data.posts[0]?.slug}`}>Read more</Link>
+      </div>
     </div>
   );
 };
